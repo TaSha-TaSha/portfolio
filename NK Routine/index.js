0c418e822538c16a3301aa3bd1fa8ab08872ee8a
塔莎 TaSha 塔莎 <96839389+TaSha-TaSha@users.noreply.github.com>
2022-08-29 09:41:08 +0300

    Update index.js

---
+++ NK Routine/index.js
@@ -0,0 +1,8 @@
+// Animation on load
+gsap.from('.containerOne', {y: -400, opacity: 0, duration: 1, delay: .5})
+gsap.from('.containerTwo', {y: -400, opacity: 0, duration: 1, delay: 1})
+gsap.from('.containerThree', {y: -400, opacity: 0, duration: 1, delay: 1.5})
+gsap.from('.containerFour', {y: -400, opacity: 0, duration: 1, delay: 2})
+gsap.from('.goBtn', {scale: 0, duration: .5, delay: 2.8, stagger: .3})
+gsap.from('.exitBtn', {scale: 0, duration: .5, delay: 4.2})
+
